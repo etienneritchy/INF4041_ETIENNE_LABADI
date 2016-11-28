9c5fe5a13bc65ce98ae773106371200bd4165386
etienneritchy <etienne.ritchy@hotmail.fr>
2016-11-28 15:31:14 +0100

    commit

--- app/src/main/java/org/esiea/labadi/etienne/myapplication/setting.java
+++ app/src/main/java/org/esiea/labadi/etienne/myapplication/setting.java
@@ -1,34 +1,60 @@
 package org.esiea.labadi.etienne.myapplication;
 
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
 import android.content.Intent;
 import android.content.res.Resources;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.app.NotificationCompat;
+import android.widget.CompoundButton;
+import android.widget.Switch;
+import android.widget.ToggleButton;
 
 public class setting extends AppCompatActivity {
+
+    public static final String BIERS_UPDATE ="com.octip.cours.inf4042_11.BIERS_UPDATE";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setting);
+        switchon();
     }
 
-/*    public void showNotification() {
+    private void switchon() {
+        Switch switch1 = (Switch) findViewById(R.id.switch1);
+        switch1.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked) {
+                    showNotification();
+                } else {
+                    // The toggle is disabled
+                }
+            }
+        });
+    }
 
-        Resources r = getResources();
-        NotificationCompat.Builder builder =
-                new NotificationCompat.Builder(this)
-                        .setSmallIcon(android.R.drawable.ic_menu_report_image)
-                        .setContentTitle(r.getString(R.string.notification_title))
-                        .setContentText(r.getString(R.string.notification_text));
 
-        Intent notificationIntent = new Intent(this, MainActivity.class);
-        PendingIntent contentIntent = PendingIntent.getActivity(this, 0, notificationIntent,
-                PendingIntent.FLAG_UPDATE_CURRENT);
-        builder.setContentIntent(contentIntent);
+    public void showNotification() {
 
-        NotificationManager notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
-        notificationManager.notify(0, builder.build());
-    }*/
+        final NotificationManager mNotification = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+
+        final Intent launchNotifiactionIntent = new Intent(this, MainActivity.class);
+        final PendingIntent pendingIntent = PendingIntent.getActivity(this,
+                0, launchNotifiactionIntent,
+                PendingIntent.FLAG_ONE_SHOT);
+
+        Notification.Builder builder = new Notification.Builder(this)
+                .setWhen(System.currentTimeMillis())
+                .setSmallIcon(R.drawable.china)
+                .setContentTitle(getResources().getString(R.string.notification_title))
+                .setContentText(getResources().getString(R.string.notification_desc))
+                .setContentIntent(pendingIntent);
+
+        mNotification.notify(0, builder.build());
+
+    }
 }
